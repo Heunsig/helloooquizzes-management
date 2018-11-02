34ff9365f356ed58230e15ce61f30b1000c0739c
Heunsig <heun3344@gmail.com>
2018-11-01 21:52:55 -0700

    os.Getenv to os.environ

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'dfasdfkaf;kjs;fasdfajds;fl'
-app.config['SQLALCHEMY_DATABASE_URI'] = os.Getenv("DATABASE_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
