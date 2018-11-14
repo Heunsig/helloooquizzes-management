257019e3438a3464e6598ef07fb01ca2eccccd21
Heunsig <heun3344@gmail.com>
2018-11-13 20:42:59 -0800

    v 0.0.4 - added Flask-SSLify

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_cors import CORS
+from flask_sslify import SSLify
 
 IS_PROD = os.environ.get('IS_HEROKU', None)
 
@@ -19,6 +20,7 @@ app.config['SECRET_KEY'] = 'dfasdfkaf;kjs;fasdfajds;fl'
 app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+sslify = SSLify(app)
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
